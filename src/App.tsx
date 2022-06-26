00e508fee92d1411632f00b95bd63756409d992d
PauloGodoiS <paulogodoi.sp@gmail.com>
2022-06-26 18:00:41 -0300

    finalizações do projeto

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { ApolloProvider } from "@apollo/client"
+import { client } from "./lib/apollo"
+import { BrowserRouter } from "react-router-dom"
+import { Router } from "./Router"
+
+
+function App() {
+  return (
+    <div>
+      <ApolloProvider client={client}>
+        <BrowserRouter>
+          <Router />
+        </BrowserRouter>
+      </ApolloProvider>
+    </div>
+  )
+}
+
+export default App
